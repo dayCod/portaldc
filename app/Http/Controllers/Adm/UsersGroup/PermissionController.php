af6930732ed632509c2e845fc8931ea27db9b5f2
wirandra.rembon <alayarahman07@gmail.com>
2024-03-02 19:39:52 +0700

    route setup

---
+++ app/Http/Controllers/Adm/UsersGroup/PermissionController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Adm\UsersGroup;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    //
+}
--- routes/adm.php
+++ routes/adm.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Adm\DashboardController;
+use App\Http\Controllers\Adm\UsersGroup\PermissionController;
+use App\Http\Controllers\Adm\UsersGroup\RoleController;
+use App\Http\Controllers\Adm\UsersGroup\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,53 @@ Route::group(['prefix' => 'adm', 'as' => 'adm.'], function () {
 
     Route::get('/', [DashboardController::class, 'index'])
         ->name('index');
+
+    // Role
+    Route::group(['prefix' => 'role', 'as' => 'role.'], function () {
+        Route::get('/', [RoleController::class, 'index'])
+            ->name('index');
+        Route::get('/create', [RoleController::class, 'create'])
+            ->name('create');
+        Route::post('/store', [RoleController::class, 'store'])
+            ->name('store');
+        Route::get('/{role}/edit', [RoleController::class, 'edit'])
+            ->name('edit');
+        Route::put('/{role}/update', [RoleController::class, 'update'])
+            ->name('update');
+        Route::delete('/{role}/delete', [RoleController::class, 'delete'])
+            ->name('delete');
+    });
+
+    // Permission
+    Route::group(['prefix' => 'permission', 'as' => 'permission.'], function () {
+        Route::get('/', [PermissionController::class, 'index'])
+            ->name('index');
+        Route::get('/create', [PermissionController::class, 'create'])
+            ->name('create');
+        Route::post('/store', [PermissionController::class, 'store'])
+            ->name('store');
+        Route::get('/{permission}/edit', [PermissionController::class, 'edit'])
+            ->name('edit');
+        Route::put('/{permission}/update', [PermissionController::class, 'update'])
+            ->name('update');
+        Route::delete('/{permission}/delete', [PermissionController::class, 'delete'])
+            ->name('delete');
+    });
+
+    // User
+    Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
+        Route::get('/', [UserController::class, 'index'])
+            ->name('index');
+        Route::get('/create', [UserController::class, 'create'])
+            ->name('create');
+        Route::post('/store', [UserController::class, 'store'])
+            ->name('store');
+        Route::get('/{user}/edit', [UserController::class, 'edit'])
+            ->name('edit');
+        Route::put('/{user}/update', [UserController::class, 'update'])
+            ->name('update');
+        Route::delete('/{user}/delete', [UserController::class, 'delete'])
+            ->name('delete');
+    });
 
 });
